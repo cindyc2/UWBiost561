6f26c63d5c55c2e4df05ca1be237152f0ad9162f
[[cindyc2]] <[[cindyc2@uw.edu]]>
2024-05-24 12:24:35 -0700

    HW 4 attempt 3

--- vignettes/hw4-demo_bayes_execute.R
+++ vignettes/hw4-demo_bayes_execute.R
@@ -9,16 +9,18 @@ alpha_vec <- c(0.5, 0.95)
 # loop over the levels
 level_trial_list <- lapply(alpha_vec, function(alpha){
   print(paste("Value of alpha:", alpha))
-  
+
   # loop over the different trials for this level
   trial_list <- lapply(1:trials, function(trial){
     print(paste("Working on trial:", trial))
     set.seed(trial) # to freeze the randomness of adj_mat
-    
+
     # generate the data
-    data <- UWBiost561::generate_partial_clique(n = 10)
+    data <- UWBiost561::generate_partial_clique(n = 10,
+                                                clique_edge_density = 0.9,
+                                                clique_fraction = 0.5)
     adj_mat <- data$adj_mat
-    
+
     # loop over the methods for this trial
     result_list <- lapply(imp_numbers, function(imp_number){
       set.seed(trial) # to freeze the randomness of the method
@@ -29,18 +31,18 @@ level_trial_list <- lapply(alpha_vec, function(alpha){
         number = imp_number,
         time_limit = 30
       )
-      
-      
+
+
       return(result)
     })
     names(result_list) <- paste("Implementation:", imp_numbers)
     cat("\n")
-    
+
     return(result_list)
   })
   names(trial_list) <- paste("Trial:", 1:trials)
   print("====")
-  
+
   return(trial_list)
 })
 names(level_trial_list) <- paste0("alpha:", alpha_vec)
