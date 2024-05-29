7e5fcc7ad9b87e0d028e000890bdba645094c621
[[cindyc2]] <[[cindyc2@uw.edu]]>
2024-05-29 07:59:05 -0700

    Attempting submit for bayes sim

---
+++ tests/testthat/test_hw4-demo_bayes_execute.R
@@ -0,0 +1,46 @@
+# create a few (more than one) unit tests for your simulation function.
+#This unit test (like your unit tests in HW3) will be in your `tests/testthat` folder.
+#I am also purposely vague about how many tests or what kind of unit tests to write.
+#This is for you to decide! After all, you're about to unleash your code to
+#perform a (potentially long) simulation test, so you hope you've tested your
+#simulation code well enough for this to be a good use of time.
+#
+# Note: Your unit tests should be <u>**fast**</u> (i.e., take no more than a minute to run).
+#This means your unit tests should <u>**not**</u> be performing your complete simulation study.
+#After all, it'll be a useless unit test if it takes an hour to figure out if
+#your unit test passed. However, you want your unit tests
+#(which take less than a minute) to give you confidence that your code will
+#work when running the complete simulation study (which might take more than an hour).
+#
+# <span style="color: blue;">(There is nothing to report for this question.
+#Your code will be in the `tests/testthat` folder, **not**
+# in this R Markdown file.)</span>
+
+
+# note if you end tests early (by pressing escape) it tells whether it passed or not so far
+
+library(testthat)
+context("Testing simulation study")
+
+#1
+test_that("hw4-demo_bayes_execute returns list", {
+  set.seed(10)
+
+  res2 <- my_function()
+
+  expect_true(is.list(res2))
+})
+#passed!
+
+
+#2
+test_that("hw4-demo_bayes_execute runs in less than 30 seconds for 1 implementation (25 minutes for full sim)", {
+  start_time <- Sys.time()
+  end_time <- Sys.time()
+
+  res2 <- my_function()
+
+  execution_time <- end_time - start_time
+  expect_true(execution_time < 1500, "Execution time should be less than 30 seconds each on average")
+})
+#passed after a long time
---
+++ vignettes/HW4_simulation_execute.R
@@ -0,0 +1,135 @@
+# Define a function that encapsulates your code
+#Copilot answer...
+
+
+#VERSION 1 Runs all 25 trials and 2 levels of alpha and 5 trials automatically
+# Define a function that encapsulates your code
+my_function <- function() {
+  rm(list = ls())
+  set.seed(10)
+  library(UWBiost561)
+
+  imp_numbers <- 1:25 # when running test that use just 1
+  trials <- 5 # when running test that use just 1
+  alpha_vec <- c(0.5, 0.95)
+
+  # Loop over the levels
+  level_trial_list <- lapply(alpha_vec, function(alpha) {
+    cat(paste("Value of alpha:", alpha, "\n"))
+
+    # Loop over the different trials for this level
+    trial_list <- lapply(1:trials, function(trial) {
+      cat(paste("Working on trial:", trial, "\n"))
+      set.seed(trial) # To freeze the randomness of adj_mat
+
+      # Generate the data
+      data <- UWBiost561::generate_partial_clique(n = 10,
+                                                  clique_edge_density = 0.9,
+                                                  clique_fraction = 0.5)
+      adj_mat <- data$adj_mat
+
+      # Loop over the methods for this trial
+      result_list <- lapply(imp_numbers, function(imp_number) {
+        set.seed(trial) # To freeze the randomness of the method
+        cat('*')
+        result <- UWBiost561::compute_maximal_partial_clique_master(
+          adj_mat = adj_mat,
+          alpha = alpha,
+          number = imp_number,
+          time_limit = 30
+        )
+
+        return(result)
+      })
+      names(result_list) <- paste("Implementation:", imp_numbers)
+      cat("\n")
+
+      return(result_list)
+    })
+    names(trial_list) <- paste("Trial:", 1:trials)
+    cat("====\n")
+
+    return(trial_list)
+  })
+  names(level_trial_list) <- paste0("alpha:", alpha_vec)
+
+  # Save the results, alpha values, date, and session info
+  date_of_run <- Sys.time()
+  session_info <- devtools::session_info()
+  save(
+    level_trial_list,
+    alpha_vec,
+    date_of_run,
+    session_info,
+    file = "~/HW4_simulation.RData"
+  )
+}
+
+# Call the function to execute your code
+# my_function()
+#
+# #VERSION 2 trying to make smaller
+#
+# my_function <- function(num_trials) {
+#   rm(list = ls())
+#   set.seed(10)
+#   library(UWBiost561)
+#
+#   imp_numbers <- 1:25
+#   trials <- num_trials
+#   alpha_vec <- c(0.5, 0.95)
+#
+#   # Loop over the levels
+#   level_trial_list <- lapply(alpha_vec, function(alpha) {
+#     cat(paste("Value of alpha:", alpha, "\n"))
+#
+#     # Loop over the different trials for this level
+#     trial_list <- lapply(1:num_trials, function(trial) {
+#       cat(paste("Working on trial:", trial, "\n"))
+#       set.seed(trial) # To freeze the randomness of adj_mat
+#
+#       # Generate the data
+#       data <- UWBiost561::generate_partial_clique(n = 10,
+#                                                   clique_edge_density = 0.9,
+#                                                   clique_fraction = 0.5)
+#       adj_mat <- data$adj_mat
+#
+#       # Loop over the methods for this trial
+#       result_list <- lapply(imp_numbers, function(imp_number) {
+#         set.seed(trial) # To freeze the randomness of the method
+#         cat('*')
+#         result <- UWBiost561::compute_maximal_partial_clique_master(
+#           adj_mat = adj_mat,
+#           alpha = alpha,
+#           number = imp_number,
+#           time_limit = 30
+#         )
+#
+#         return(result)
+#       })
+#       names(result_list) <- paste("Implementation:", imp_numbers)
+#       cat("\n")
+#
+#       return(result_list)
+#     })
+#     names(trial_list) <- paste("Trial:", 1:num_trials)
+#     cat("====\n")
+#
+#     return(trial_list)
+#   })
+#   names(level_trial_list) <- paste0("alpha:", alpha_vec)
+#
+#   # Save the results, alpha values, date, and session info
+#   date_of_run <- Sys.time()
+#   session_info <- devtools::session_info()
+#   save(
+#     level_trial_list,
+#     alpha_vec,
+#     date_of_run,
+#     session_info,
+#     file = "~/demo_simulation.RData"
+#   )
+# }
+
+# Call the function to execute your code
+#my_function()
